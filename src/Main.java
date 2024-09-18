04bcf2544c819628e72a195b035c9d6e2627add6
JoeyIgbinoba <146754256+JoeyIgbinoba@users.noreply.github.com>
2024-09-18 13:20:13 -0400

    Trying a new one
    trys

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,10 @@ public class Main {
 		String name = in.nextLine();
 		System.out.println("What animal pops into you head? When you think of the word flight.");
 		String animal = in.nextLine();
-		System.out.println("This animal is being hunted by a 2 legged creature");
+		System.out.println("This animal is being hunted by an that walks what is it");
 		String book = in.nextLine();
+		System.out.println("Give me a name");
+		String hunterN = in.nextLine();
 		System.out.println(name+" the " +animal+ "is being hunted by the Jack the Hunter of Tousand Tiger Tails");
 		System.out.println("During this time Jack the hunter teaches chases the creatures and reads the book called " +book+ ".");
 
